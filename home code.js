4c9d265f5010e0f097b26928e0906706f5eb6c97
DarleyArcaya <153260033+DarleyArcaya@users.noreply.github.com>
2024-09-10 23:51:07 -0400

    Add files via upload

---
+++ home code.js
@@ -0,0 +1,9 @@
+function stillworking() {
+    alert('This is a beta page, please be patient while we working on it.')
+}
+stillworking() 
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,4 @@
+function saludos() {
+    alert('Bienvenido a la beta, no tiene soporte aun para celular, activa el modo escritorio')
+}
+saludos()
